0f0106699d47b614dd72d9675721b0ad646ed907
zanqwq <zanqwq@163.com>
2020-05-16 21:42:05 +0800

    add a header to NotFound page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="text-center">
-    <h3>Page not found.</h3>
-    <p>
-      This page may be private.
-    </p>
+  <div>
+    <Header />
+    <div class="text-center" style="margin-top: 5rem">
+      <h3>Page not found.</h3>
+      <p>
+        This page may be private.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header
+  }
+};
 </script>
 
 <style></style>
