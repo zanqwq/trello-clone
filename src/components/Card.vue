dfe03c1f249c1c37bb15e67daf12f3b578699df8
zanqwq <zanqwq@163.com>
2020-05-16 15:29:21 +0800

    extracted and refactor from Board.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,78 @@
 <template>
-  <div class="card">
-    <div class="card-content"></div>
+  <div
+    class="card"
+    draggable="true"
+    @dragstart.self="$emit('drag-start', $event)"
+    @dragenter.prevent
+    @dragover.prevent
+    @drop.prevent="$emit('drop-on-card', $event)"
+  >
+    <div>
+      {{ title }}
+
+      <b-button
+        class="float-right remove-card"
+        variant="danger"
+        size="sm"
+        @click="$emit('remove-card')"
+      >
+        <b-icon icon="trash" />
+      </b-button>
+
+      <b-button
+        class="float-right mr-1 edit-card"
+        variant="success"
+        size="sm"
+        @click="$emit('edit-card')"
+      >
+        <b-icon icon="pencil"></b-icon>
+      </b-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    setCardDataForModal() {
+      this.$root.$emit("bv::show::modal", "modal");
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  background: var(--white);
+  transition: all 0.3s ease-in-out;
+  overflow: hidden;
+}
+
+.remove-card,
+.edit-card {
+  opacity: 0;
+  transform: translateX(20px);
+  transition: all 0.3s ease-in-out;
+}
+
+.card:hover {
+  background: #e9e9e9;
+  cursor: pointer;
+}
+
+.card:hover .remove-card,
+.card:hover .edit-card {
+  opacity: 1;
+  transform: translateX(0);
+}
+</style>
