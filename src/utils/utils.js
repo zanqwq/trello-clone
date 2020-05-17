8f63b1905b8df04e58fb245041a232fc1dd0f2f7
zanqwq <zanqwq@163.com>
2020-05-17 12:19:09 +0800

    add implement save state plugin for store

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,12 @@
 export function uid() {
-  // 生成随机唯一 id
+  // generate random unique id
   return Math.random()
     .toString(16)
     .slice(2);
 }
 
-// export function saveStatePlugin(store) {
-//   store.subscribe(
-//     (mutation, state) => {
-//       localStorage.setItem(undefined, JSON.stringify(state));
-//     }
-//   );
-// }
+export function saveStatePlugin(store) {
+  store.subscribe((mutation, state) => {
+    localStorage.setItem(state.email, JSON.stringify(state.user));
+  });
+}
